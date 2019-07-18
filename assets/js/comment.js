c3e26fabc975b44248ea7e5e1a4b00a61e2c520a
DePrism <1696936797@qq.com>
2019-07-18 23:16:25 +0800

    Gitalk

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -1,30 +1,13 @@
-const myTheme = {
-	render(state, instance) {
-		const container = document.createElement('div');
-		container.lang = "zh-CN";
-		container.className = 'gitment-container gitment-root-container';
-		container.appendChild(instance.renderHeader(state, instance));
-		container.appendChild(instance.renderComments(state, instance));
-		container.appendChild(instance.renderEditor(state, instance));
-		container.appendChild(instance.renderFooter(state, instance));
-		return container;
-	}
-}
+var gitalk = new Gitalk({
+  clientID: 'b3ea4fecb63727152102',
+  clientSecret: '5879d4df4b5b5aa1a95a91de6d7236819fb86650',
+  repo: 'deprism.github.io',
+  owner: 'DePrism',
+  admin: ['DePrism'],
+  id: location.pathname,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false,  // Facebook-like distraction free mode
+  language: 'zh-CN',
+  title: 'Feedback'
+})
 
-function showGitment() {
-	$("#gitment_title").attr("style", "display:none");
-	$("#container").attr("style", "").addClass("gitment_container");
-	var gitment = new Gitment({
-		id: decodeURI(window.location.pathname),
-			theme: myTheme,
-			owner: 'DePrism',
-			repo: 'deprism.github.io',
-			oauth: {
-				client_id: 'b3ea4fecb63727152102',
-				client_secret: '5879d4df4b5b5aa1a95a91de6d7236819fb86650'
-			}
-	});
-	gitment.render('container');
-}
-
-showGitment();
+gitalk.render('gitalk-container')
