5cf8697b97b256decf0e93d860782976587c02d4
DepletedPrism <depletedprism@qq.com>
2020-03-28 22:23:36 +0800

    gitalk fix

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -2,7 +2,7 @@ var gitalk = new Gitalk({
   clientID: '49032d909f27598e5c2b',
   clientSecret: 'bf1bc1c933f6a025485bb0233b32a395466f4691',
   repo: 'blackholeproject.github.io',
-  owner: 'DepletedPrism',
+  owner: 'BlackholeProject',
   admin: ['DepletedPrism', 'aghinouz'],
   id: location.pathname,
   distractionFreeMode: false,
